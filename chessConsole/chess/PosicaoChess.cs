2b9e47baa3000baa51f1fd4ce38a5f08fa6849e6
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-22 12:40:08 -0300

    classe 'PosicaoChess'

---
+++ chessConsole/chess/PosicaoChess.cs
@@ -0,0 +1,26 @@
+﻿using chessConsole.tabuleiro;
+
+namespace chessConsole.chess
+{
+    internal class PosicaoChess
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoChess(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+                return "" + coluna + linha;
+        }
+    }
+}
