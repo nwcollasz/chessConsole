1faa1d075fc8ab3842116e43729a530893bdf140
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-22 12:25:24 -0300

    uso de exception

--- chessConsole/Program.cs
+++ chessConsole/Program.cs
@@ -8,10 +8,18 @@ namespace chessConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(0, 0));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 9));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ chessConsole/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace chessConsole.tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
