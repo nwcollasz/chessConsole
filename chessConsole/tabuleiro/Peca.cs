8afde116d21bf8b207bb14651c584505fff2f953
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-29 20:24:17 -0300

    lógica de xeque

--- chessConsole/tabuleiro/Peca.cs
+++ chessConsole/tabuleiro/Peca.cs
@@ -22,6 +22,11 @@ namespace chessConsole.tabuleiro
             qteMovimentos++;
         }
 
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis()
         {
             bool[,] mat = movimentosPossiveis();
